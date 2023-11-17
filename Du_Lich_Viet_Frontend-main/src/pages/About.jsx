f369fc124e0795539502677d14b6088a65696cf8
vanvunguyen2k2 <121481664+vanvunguyen2k2@users.noreply.github.com>
2023-11-17 23:17:10 +0700

    Delete Du_Lich_Viet_Frontend-main directory

--- Du_Lich_Viet_Frontend-main/src/pages/About.jsx
+++
@@ -1,6 +0,0 @@
-const About = () => {
-    return(
-        <></>
-    )
-}
-export default About;
--- Du_Lich_Viet_Frontend-main/src/pages/Service.jsx
+++
@@ -1,6 +0,0 @@
-const Service = () => {
-    return(
-        <></>
-    )
-}
-export default Service;
